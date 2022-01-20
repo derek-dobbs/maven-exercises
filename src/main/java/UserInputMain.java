a325307a3b354c56abca0009eced0b1910187207
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-20 15:35:39 -0600

    Fixed formatting for prompt and removed unnecessary comments

--- src/main/java/UserInputMain.java
+++ src/main/java/UserInputMain.java
@@ -1,22 +1,17 @@
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Scanner;
 
 public class UserInputMain {
     public static void main(String[] args) {
-//        System.out.println("Test UserInput class");
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
 
-        System.out.print("Enter something: ");
+        System.out.print("Enter Something: ");
         String userEntry = scanner.next();
-//        System.out.println("You entered: " + userEntry);
-        System.out.printf("You entered \"%s\"", userEntry);
 
-        //Tells whether or not what the user entered is a number
-        //IsAlpha/IsNumeric/IsWhitespace/IsAsciiPrintable - checks the characters in a String, use isNumeric
+        System.out.printf("You Entered \"%s\"", userEntry);
+
         if(StringUtils.isNumeric(userEntry)) {
-//            System.out.println(userEntry + " is a number");
             System.out.printf("%n\"%s\" is a number", userEntry);
         }else {
             System.out.printf("%n\"%s\" is not a number", userEntry);
@@ -24,7 +19,6 @@ public class UserInputMain {
 
         //Flips the case of the string
         String flippedCase = StringUtils.swapCase(userEntry);
-
         System.out.printf("%nFlipped Case: %s", flippedCase);
 
         //Reverses the string
