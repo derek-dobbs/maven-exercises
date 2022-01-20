e226019a838492e081f507b1c992915cc08f90d4
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-20 15:31:59 -0600

    Completed exercise

--- src/main/java/UserInputMain.java
+++ src/main/java/UserInputMain.java
@@ -10,20 +10,25 @@ public class UserInputMain {
 
         System.out.print("Enter something: ");
         String userEntry = scanner.next();
-        System.out.println("You entered: " + userEntry);
+//        System.out.println("You entered: " + userEntry);
+        System.out.printf("You entered \"%s\"", userEntry);
 
         //Tells whether or not what the user entered is a number
         //IsAlpha/IsNumeric/IsWhitespace/IsAsciiPrintable - checks the characters in a String, use isNumeric
         if(StringUtils.isNumeric(userEntry)) {
-            System.out.println(userEntry + " is a number");
+//            System.out.println(userEntry + " is a number");
+            System.out.printf("%n\"%s\" is a number", userEntry);
         }else {
-            System.out.println(userEntry + " is not a number.");
+            System.out.printf("%n\"%s\" is not a number", userEntry);
         }
 
         //Flips the case of the string
         String flippedCase = StringUtils.swapCase(userEntry);
 
-        System.out.printf("Flipped Case: %s", flippedCase);
+        System.out.printf("%nFlipped Case: %s", flippedCase);
+
         //Reverses the string
+        String reversedString = StringUtils.reverse(userEntry);
+        System.out.printf("%nReversed: %s", reversedString);
     }// end main
 }// end class
